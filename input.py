d1c00b6aeb9c24d71f0c21d6f0b043b00f3216eb
s.shahraeini@yahoo.com <s.shahraeini@yahoo.com>
2022-11-28 17:32:21 +0330

    Initial commit

---
+++ input.py
@@ -0,0 +1,22 @@
+### input() --> baraye gereftane voroodi az user
+# name = "Samaneh"
+# name = input("What is your name?")
+# print(name)
+# last_name = input("What is your last name?")
+# # print("Hello " + name + " " + last_name)
+# print(f"Hello {name} {last_name}")
+#-------------------
+# خروجی input از نوع رشته می باشد
+# age = input("What is your age?")
+# print(type(age)) # str
+# next_10_years = age + 10  ---> TypeError : str + int
+# print(next_10_years)
+#---------------------------------
+# int()
+age = int(input("What is your age?"))
+print(type(age)) # int
+next_10_years = age + 10
+print(next_10_years)
+
+
+
